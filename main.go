4f193d11e0d9198f8c747432a483aad3648ffa59
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-01-18 17:18:12 +0800

    Feat: can accept args

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	url := os.Args[1]
+
+	if url == "" {
+		fmt.Println("URL not provided")
+		os.Exit(1)
+	}
+
 	fmt.Println("Hello world")
 }
