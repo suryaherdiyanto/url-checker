d8d106e492f553b859613bf5718129dee88d57ce
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-01-18 17:25:28 +0800

    Feat: input validation

--- main.go
+++ main.go
@@ -13,5 +13,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	if len(url) <= 8 {
+		fmt.Println("Might not a valid URL")
+		os.Exit(1)
+	}
+
+	if url[:8] != "https://" && url[:7] != "http://" {
+		fmt.Println("URL is invalid")
+		os.Exit(1)
+	}
+
 	fmt.Println("Hello world")
 }
